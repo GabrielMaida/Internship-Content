4b9d7ac56510cca4f4581f3c8a5124d4a8cf9438
Gabriel Antônio <gabriel.antonio.dlm@gmail.com>
2025-04-14 11:58:48 -0300

    add: New typescript learning folder added and configured

---
+++ TypeScript/TypeScript.Basics/app.ts
@@ -0,0 +1,7 @@
+console.log("Hello World!\n");
+
+let oi:number = 4;
+
+oi += 3;
+
+console.log(oi);
---
+++ TypeScript/TypeScript.Basics/dist/app.js
@@ -0,0 +1,5 @@
+console.log("Hello World!\n");
+let oi = 4;
+oi += 3;
+console.log(oi);
+export {};
