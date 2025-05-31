c3d2a18aa9c774b2471c74c56a892f4bea53954f
Gabriel Antônio <gabriel.antonio.dlm@gmail.com>
2025-05-31 20:16:54 -0300

    update: new content

---
+++ TypeScript/index.ts
@@ -0,0 +1,26 @@
+/*type Pokemon = {
+    pokedex_id: number,
+    name: string,
+    type: string,
+    message: string,
+    height: number,
+    weight: number
+}
+
+const pokemon1: Pokemon =  {
+    pokedex_id: 1,
+    name: "Bulbasaur",
+    type: "Grass/Poison",
+    message: "A strange seed was planted on its back at birth. The plant sprouts and grows with this Pokémon.",
+    height: 0.7,
+    weight: 6.9
+}
+
+console.log(pokemon1);*/
+
+function add(a: number, b: number) {
+	var result: number = a + b;
+	return result;
+}
+
+console.log(typeof add(2, 5));
